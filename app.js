ae67ec101113dd4923687e00c2645d5a7221ea59
Joseph VanWagoner <joseph.vanwagoner@chghealthcare.com>
2020-03-19 08:08:58 -0600

    First Commit

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const fs = require("fs");
+const serveIndex = require("serve-index");
+const sharp = require("sharp");
+const cors = require("cors");
+
+const app = express();
+const PORT = 4000;
+
+app.use(cors());
+app.options('*', cors());
+
+app.use('/images', express.static('public/images'));
+app.use('/images', serveIndex('public/images'));
+
+app.get('/', (req, res) =>
+    res.send(`Node and express server is running on port ${PORT}`)
+);
+
+app.get('/before', (req, res) => {
+    fs.promises.readdir("./public/images").then(files => {
+        res.send(files);
+    });
+});
+
+app.get('/shrinker/:path', (req, res) => {
+    let fileName = req.params.path;
+    const fileList = fileName.split(".");
+    const ext = fileList.pop();
+    const nameList = fileList[0].split("~");
+    const width = parseInt(nameList.pop());
+    fileName = `${nameList[0]}.${ext}`;
+    sharp(`./public/images/${fileName}`)
+        .resize(width)
+        .toBuffer()
+        .then(data => {
+            res.contentType(ext);
+            res.send(data);
+        });
+});
+
+app.listen(PORT, () =>
+    console.log(`your server is running on port ${PORT}`)
+);
