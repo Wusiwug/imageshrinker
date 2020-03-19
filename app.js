08b68206f378a12db3656784f2f2f20b7e9bb9c3
Joseph VanWagoner <joseph.vanwagoner@chghealthcare.com>
2020-03-19 09:55:01 -0600

    3/19/20

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const sharp = require("sharp");
 const cors = require("cors");
 
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 app.use(cors());
 app.options('*', cors());
